80e04744d9bec847ff3644b97264bfa3dc558234
LuizGustavo <gustavol2010@yahoo.com>
2022-03-24 10:43:19 -0300

    3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import './App.css';
 import tmdb from './tmdb';
 import MovieRow from './components/MovieRow';
 
--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -12,7 +12,7 @@ export default ({ title, items }) => {
 
                 {items.results.length > 0 && items.results.map((item, key) => (
                   
-                  <img src={'https://image.tmdb.org/t/p/w300${item.poster_path}'} />
+                  <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}alt={item.Original_title}/>
 
                 ))}
 
